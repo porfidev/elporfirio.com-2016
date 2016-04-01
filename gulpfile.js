13de11a7e9a46ee3dd7e4f16407c453353beeda7
elporfirio <elporfirio@gmail.com>
2016-03-31 22:57:28 -0600

    Se agrega bower, angular y jquery

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,14 @@
  * Created by elporfirio on 31/03/16.
  */
 var gulp        = require('gulp');
+var sass        = require('gulp-ruby-sass');
 var browserSync = require('browser-sync').create();
+
+
+var config = {
+  sassPath: './resources/sass',
+  bowerDir: './bower_components'
+};
 
 // Static server
 gulp.task('browser-sync', function() {
@@ -11,4 +18,31 @@ gulp.task('browser-sync', function() {
       baseDir: "./app"
     }
   });
+});
+
+gulp.task('css', function() {
+  return gulp.src(config.sassPath + '/style.scss')
+    .pipe(sass({
+      style: 'compressed',
+      loadPath: [
+        //'./resources/sass',
+        config.bowerDir + '/bootstrap-sass/assets/stylesheets'
+        //config.bowerDir + '/fontawesome/scss'
+      ]
+    })).pipe(gulp.dest('./app/css'));
+      // .on("error", notify.onError(function (error) {
+      //   return "Error: " + error.message;
+      // })))
+
+});
+
+
+gulp.task('sass', function () {
+  return sass({
+    loadPath: [
+      './bower_components/bootstrap-sass/assets/stylesheets/_bootstrap.scss'
+    ]
+  })
+    .on('error', sass.logError)
+    .pipe(gulp.dest('result'));
 });
